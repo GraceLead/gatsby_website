d27691894477f453e1c63c159eb7d8709c1a51dd
djruess <david@gracelead.org>
2020-05-05 19:21:05 -0500

    Update siteConfig.js with Grace Lead Info

--- siteConfig.js
+++ siteConfig.js
@@ -17,8 +17,37 @@ module.exports = {
     shareImageHeight: 523,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
-    shortTitle: `Jamify`,
+    shortTitle: `Grace Lead`,
     siteIcon: `favicon.png`,
     backgroundColor: `#e9e9e9`,
     themeColor: `#15171A`,
+}
+
+//siteConfig.js
+module.exports = {
+    // Do not include a trailing slash!
+    siteUrl: `https://gracelead.org`,
+
+    // Maximum number of post shown per page
+    // Infinite Scroll: Initial chunk of posts, subsequent posts are fetched one by one
+    postsPerPage: 3, 
+
+    // This allows an alternative site title for meta data for pages.
+    siteTitleMeta: `Grace Lead`, 
+
+    // This allows an site description for meta data for pages.
+    siteDescriptionMeta: `Your Partner In The Gospel`, 
+
+    // Change to the width and height of your default share image
+    shareImageWidth: 1000, 
+    shareImageHeight: 523,
+
+    // Used for App and Offline manifest e.g. Mobile Home Screen
+    shortTitle: `Grace Lead`, 
+    siteIcon: `favicon.png`, 
+    backgroundColor: `#e9e9e9`,
+    themeColor: `#15171A`,
+
+    // Overwrite navigation menu (default: []), label is case sensitive
+    // overwriteGhostNavigation: [{ label: `Home`, url: `/` }],
 }
